9f21147fc6c964af79617e5fae9435afc9a44329
Vicent Sanjaime <vsanjaime@cartodb.com>
2025-04-29 15:59:00 +0200

    chore: empty schools list

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,15 +1,23 @@
 import './Results.css';
 import { School } from '../types/types';
 import { CardSchool } from './CardSchool';
+import noResults from '../assets/icons/no_results.svg';
 
 export function Results({ schools }: { schools: School[] }) {
     return (
         <>
         <div className="results">            
             <h2>Listado de colegios ordenados</h2>
-            {schools.map((school) => (
-                <CardSchool key={school.Codigo} school={school} />
-            ))}
+            {schools.length === 0 ? (
+                <div className="no-results">
+                    <img src={noResults} alt="" />
+                    <p>No se han encontrado colegios con los criterios seleccionados</p>
+                </div>
+            ) : (
+                schools.map((school) => (
+                    <CardSchool key={school.Codigo} school={school} />
+                ))
+            )}
         </div>
         </>
     )
